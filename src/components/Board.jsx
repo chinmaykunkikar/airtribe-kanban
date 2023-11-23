4fe34634140b8efe872e4196f866c7dd177d1695
Chinmay Kunkikar <chinmay.kunkikar@gmail.com>
2023-11-23 21:57:03 +0530

    Use Popover for adding new status

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -3,10 +3,18 @@ import { useContext, useState } from "react";
 import AppContext from "../contexts/AppContext";
 import DetailsPage from "./Details";
 import Status from "./Status";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { CheckIcon } from "lucide-react";
 
 export default function Board() {
   const { state, dispatch } = useContext(AppContext);
   const [droppable, setDroppable] = useState(false);
+  const [closePop, setClosePop] = useState(false);
+  const [statusName, setStatusName] = useState("");
 
   const selectedTaskId = state.selectedTaskId;
 
@@ -32,7 +40,7 @@ export default function Board() {
   }
 
   return (
-    <div className="flex">
+    <div className="flex items-start">
       {selectedTaskId ? (
         <DetailsPage taskId={selectedTaskId} />
       ) : (
@@ -49,18 +57,39 @@ export default function Board() {
               />
             ))}
           </div>
-          <button
-            className="rounded px-2 text-neutral-400 hover:bg-neutral-100"
-            title="Add a new status"
-            onClick={() => {
-              const newStatusName = prompt("Type new status name");
-              if (newStatusName) {
-                addNewStatus(newStatusName);
-              }
-            }}
-          >
-            <PlusIcon className="h-6 w-6 stroke-2" />
-          </button>
+          <Popover modal open={closePop}>
+            <PopoverTrigger
+              onClick={() => setClosePop(true)}
+              className="mt-4 rounded p-2 text-neutral-400 hover:bg-neutral-100"
+              title="Add a new status"
+            >
+              <PlusIcon className="h-6 w-6 stroke-2" />
+            </PopoverTrigger>
+            <PopoverContent className="flex max-w-[16rem] shadow-xl">
+              <input
+                autoFocus
+                type="text"
+                name="new-status"
+                id="new-status"
+                placeholder="New status"
+                className="w-full focus:outline-none"
+                value={statusName}
+                onChange={(e) => setStatusName(e.target.value)}
+              />
+              <button
+                disabled={statusName === ""}
+                onClick={() => {
+                  addNewStatus(statusName);
+                  setStatusName("");
+                  setClosePop(false);
+                }}
+                title="Add status"
+                className="rounded border bg-white shadow-md hover:bg-neutral-200 disabled:bg-neutral-200"
+              >
+                <CheckIcon className="h-6 w-6 stroke-neutral-400 stroke-2" />
+              </button>
+            </PopoverContent>
+          </Popover>
         </>
       )}
     </div>
